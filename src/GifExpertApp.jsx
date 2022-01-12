3317d9d47e2288282401977e0904facc9fb81aa9
gwynlordofcinder <pabloandrestejedor@icloud.com>
2022-01-11 23:03:01 -0300

    default search added

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,7 +3,7 @@ import { AddCategory } from "./components/AddCategory";
 import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(['fight club', 'goodfells', 'the goodfather', 'inglorious basterds']);
 
   return (
     <>
